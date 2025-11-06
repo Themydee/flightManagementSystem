f76a8294e04fe48df854f3f1cc0a764772fb7638
Themydee <nifetemiboy@gmail.com>
2025-11-06 17:34:40 +0100

    fixes

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:3000/api';
+const API_BASE_URL = 'https://flight-management-system-ten.vercel.app/api';
 
 // Create Axios instance
 const api = axios.create({
