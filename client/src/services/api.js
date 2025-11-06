4d1e43d2fc3477b786495566ad4f9df7cc396f3e
Themydee <nifetemiboy@gmail.com>
2025-11-06 17:51:06 +0100

    local

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://flightmanagementsystem.onrender.com/api';
+const API_BASE_URL = 'http://localhost:3000/api';
 
 // Create Axios instance
 const api = axios.create({
